715b73ef8cb846c1998694a453a950fd765be506
Rohit <dhangarrohit5276@gmail.com>
2026-02-19 23:55:53 +0530

    Update codemine.cpp

--- codemine.cpp
+++ codemine.cpp
@@ -1,8 +1,6 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    int a = 10
-    cout << a << endl;
-    return 0;
+public class Main {
+    public static void main(String[] args) {
+        int a = 10
+        System.out.println(a);
+    }
 }
