6da5bebe1463086c3429ced8106a2336c9c0c59b
Rohit <dhangarrohit5276@gmail.com>
2026-02-20 02:06:23 +0530

    Update codemine.java

--- codemine.java
+++ codemine.java
@@ -1,6 +1,5 @@
-public class Main {
+public class Test1 {
     public static void main(String[] args) {
-        int a = 10
-        System.out.println(a);
+        System.out.println("Hello World")
     }
 }
